92ee9f244dd17f8dec04b26115f2201503d7c9cc
Thomas Brosch <broschi@posteo.de>
2024-12-06 18:30:15 +0100

    First version of my own keymapping: - Win and Mac layer:   - Change order of modifiers to Ctrl Alt GUI Space GUI Alt Ctrl Fn - Mac layer:   - Bring the function keys to the default layer and media keys to Fn layer.   - Add mod-taps similar to home row mods to row below home row.   - Send Print Screen keycode on Screen Snapshot key to make the Mac layer more fitting for Linux. I want to use that layer as my home row mod layer for multiple OSes.

---
+++ keyboards/keychron/v3_max/ansi_encoder/keymaps/kickrunner/config.h
@@ -0,0 +1,8 @@
+// Configure the global tapping term (default: 200ms)
+#define TAPPING_TERM 200
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
+// Disable activating the on hold feature of a mod-tap key when another key is pressed before tap-term runs out.
+// #define HOLD_ON_OTHER_KEY_PRESS 0
